9f88c20aa35b9dc72048e2d26017ab6435c6b3a9
Mathias Hølestøl <mathiash@uia.no>
2024-03-18 11:45:49 +0100

    Added negative tests to DeckOfCardsTest class

--- src/test/java/idatt2003/DeckOfCardsTest.java
+++ src/test/java/idatt2003/DeckOfCardsTest.java
@@ -31,9 +31,43 @@ public class DeckOfCardsTest {
         DeckOfCards deck = new DeckOfCards();
         assertEquals("H2", deck.getDeck('H', 2).getAsString());
         assertEquals("D1", deck.getDeck('D', 1).getAsString());
+        assertEquals("C13", deck.getDeck('C', 13).getAsString());
+        assertEquals("S11", deck.getDeck('S', 11).getAsString());
+        assertEquals("H12", deck.getDeck('H', 12).getAsString());
       } catch (Exception e) {
         fail("An exception was thrown with the message: " + e.getMessage());
       }
     }
+
+
+  }
+
+  @Nested
+  @DisplayName("Negative tests")
+  class NegativeTests {
+
+      @Test
+      @DisplayName("getDeck throws IllegalArgumentException when given invalid suit.")
+      void getDeckThrowsExceptionOnInvalidSuit() {
+        try {
+          DeckOfCards deck = new DeckOfCards();
+          deck.getDeck('K',10);
+          fail("An exception was not thrown");
+        } catch (Exception e) {
+          assertEquals(e.getMessage(), "Parameter suit must be one of H, D, C or S");
+        }
+      }
+
+      @Test
+      @DisplayName("getDeck throws IllegalArgumentException when given invalid face.")
+      void getDeckThrowsExceptionOnInvalidFace() {
+        try {
+          DeckOfCards deck = new DeckOfCards();
+          deck.getDeck('H',15);
+          fail("An exception was not thrown");
+        } catch (Exception e) {
+          assertEquals(e.getMessage(), "Parameter face must be a number between 1 to 13");
+        }
+      }
   }
 }
